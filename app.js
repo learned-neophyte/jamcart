4574a83a7949075adba52515f1f6a27c33b6354f
siddhant <dmtrg69@gmail.com>
2020-12-01 11:33:02 +0530

    add files to gitignore ready for heroku trial 1

--- app.js
+++ app.js
@@ -209,4 +209,8 @@ app.post( "/delete-from-cart", ( req, res) => {
     });
 });
 
-app.listen( process.env.PORT || 3000, () => console.log("http://localhost:3000"))
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen( port, () => console.log("server started successfully."));
