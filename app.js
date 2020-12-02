66905c557759fa058347c04b88f00f91e23e6bfb
siddhant <dmtrg69@gmail.com>
2020-12-02 16:26:20 +0530

    change jamcart-logo in signup page

--- app.js
+++ app.js
@@ -213,4 +213,4 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen( 3000, () => console.log("server started successfully."));
+app.listen( port, () => console.log("server started successfully."));
